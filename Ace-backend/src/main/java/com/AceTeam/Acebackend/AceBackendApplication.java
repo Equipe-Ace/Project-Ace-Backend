9c2a149705779f7ff25e8950a19e85021986f6cd
Caio <caiovitordias1@gmail.com>
2023-03-07 19:04:28 -0300

    geraçao do projeto spring e organizacao de pastas

---
+++ Ace-backend/src/main/java/com/AceTeam/Acebackend/AceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.AceTeam.Acebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceBackendApplication.class, args);
+	}
+
+}
---
+++ Ace-backend/src/test/java/com/AceTeam/Acebackend/AceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AceTeam.Acebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
